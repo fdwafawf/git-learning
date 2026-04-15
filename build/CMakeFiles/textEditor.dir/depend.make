221f04e4f2ee49e3a5b194024b82e15e905417a1
fdwafawf <karamanleshwarner6@gmail.com>
2026-04-15 17:37:08 +0800

    first commit

---
+++ build/CMakeFiles/textEditor.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for textEditor.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/textEditor.dir/main.c.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/textEditor.dir/main.c.obj: D:\A_Projects\new\main.c \
+ C:/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/mingw64/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h
